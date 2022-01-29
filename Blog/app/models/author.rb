72c8acd3e72079dbb83838e636581b153b15644f
Kate Haman <11kateh@gmail.com>
2022-01-29 23:07:55 +0200

    updated styling, refactored code a bit

--- Blog/app/models/author.rb
+++ Blog/app/models/author.rb
@@ -13,6 +13,7 @@ class Author < ApplicationRecord
     (?=.*[[:^alnum:]])
 /x.freeze
 
+  validates :first_name, :last_name, presence: true
   validates_uniqueness_of :email
   validates_format_of :email, with: /\A[\w.+-]+@\w+\.\w+\z/, message: 'Please enter a valid email address.'
   validates :password, format: PASSWORD_REQUIREMENTS, allow_nil: true
