521aad60299356630d8a84309dc190ac93d0c419
Kate Haman <11kateh@gmail.com>
2022-05-01 13:42:19 +0200

    removed string images column from the DB

---
+++ Blog/db/migrate/20220501114026_remove_image_from_authors.rb
@@ -0,0 +1,5 @@
+class RemoveImageFromAuthors < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :authors, :image
+  end
+end
--- Blog/db/schema.rb
+++ Blog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_190614) do
+ActiveRecord::Schema.define(version: 2022_05_01_114102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2022_01_31_190614) do
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
-    t.index %w[blob_id variation_digest], name: "index_active_storage_variant_records_uniqueness", unique: true
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "author_comment_votes", force: :cascade do |t|
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(version: 2022_01_31_190614) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
     t.string "password_digest"
-    t.string "image"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -92,7 +91,6 @@ ActiveRecord::Schema.define(version: 2022_01_31_190614) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "author_id", null: false
